c3fe49b4735a9213b0ae2924d6891874cede179f
Hamzak4 <hamxak441@gmail.com>
2026-06-27 14:37:35 +0500

    Updated for axios file

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,9 +15,7 @@ const PORT=process.env.PORT|| 5001;
 const __dirname=path.resolve()
 
 if(process.env.NODE_ENV !=="production"){
-app.use(cors({
-    origin:  "http://localhost:5173",
-}));
+app.use(cors());
 }
 
 app.use(express.json());
